7194d72a77ea76d9815c63a522585b3a837806ea
Jude Ocheje <ochejejude@gmail.com>
2022-09-13 16:54:00 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Check description
+ *
+ * Description: It prints the alphabets in lower case 10 times, followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char words[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	int j = 0;
+
+	while(j < 10)
+	{
+		for (i = 0; i < 26; i++)
+	        {	
+		     _putchar(words[i]);
+	        }
+
+		_putchar('\n');
+
+		j += 1;
+
+	}
+
+}
