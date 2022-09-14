5ef0d03b13d3521c3d7858e888c9690ea9444321
Jude Ocheje <ochejejude@gmail.com>
2022-09-13 17:09:40 -0700

    Fix betty errors

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Check description
+ * print_alphabet_x10 - Check description
  * Description: It prints the alphabets in lower case 10 times,
  * followed by a new line
  * Return: Nothing.
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 	int i;
 	int j = 0;
 
-	while(j < 10)
+	while (j < 10)
 	{
 		for (i = 0; i < 26; i++)
 		{
