0392b62672e96dba1bb8ec6ee08110700f6d40fb
Jude Ocheje <ochejejude@gmail.com>
2022-09-13 17:57:54 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - Check Description
+ * @i: The integer to check
+ * Description: It returns the absolute value of an integer
+ * Return:  absolute value of integer i
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
