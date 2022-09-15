e2f1785c99dc3b022f3345f0ef2caff0047e3514
Jude Ocheje <ochejejude@gmail.com>
2022-09-15 10:37:11 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+
+/**
+ * mul - Check Description
+ * @a: The first integer parameter
+ * @b: The second integer parameter
+ *
+ * Description: It nultiplies two integers
+ * Return: The result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
