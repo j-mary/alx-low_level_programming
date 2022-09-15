28825caf6514484d9549f092717940fd5b2b452f
Jude Ocheje <ochejejude@gmail.com>
2022-09-15 10:23:50 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+
+/**
+ * _isupper - Check Description
+ * @c: The character to be checked
+ *
+ * Description: It checks for upper case character 
+ *
+ * Return: 1 if the character is uppercase. 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
